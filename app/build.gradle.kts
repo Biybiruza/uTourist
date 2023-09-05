ba3a93b4f4a9e3d96912108e6556eb3c0f8ea1f1
Biybiruza <ibashovabiybi@gmail.com>
2023-09-05 15:09:51 +0500

    add languages function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     //otp view
     implementation("io.github.chaosleung:pinview:1.4.4")
 
+    //localization language
+//    implementation("com.akexorcist:localization:1.2.11")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/com/biybiruza/myapplication/ui/languages/LanguageFragment.kt
+++ app/src/main/java/com/biybiruza/myapplication/ui/languages/LanguageFragment.kt
@@ -1,15 +1,19 @@
 package com.biybiruza.myapplication.ui.languages
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.biybiruza.myapplication.R
 import com.biybiruza.myapplication.databinding.FragmentLanguageBinding
+import java.util.Locale
 
-class LanguageFragment : Fragment(R.layout.fragment_language) {
+class LanguageFragment : Fragment(R.layout.fragment_language){
 
     private lateinit var binding: FragmentLanguageBinding
+
 
     private var isChecked = false
 
@@ -19,6 +23,8 @@ class LanguageFragment : Fragment(R.layout.fragment_language) {
 
         binding.apply {
             cvEnglish.setOnClickListener {
+                setLocate("en")
+
                 findNavController().navigate(R.id.action_languageFragment_to_onBoardingFragment)
             }
             cvKarakalpak.setOnClickListener {
@@ -28,8 +34,31 @@ class LanguageFragment : Fragment(R.layout.fragment_language) {
                 findNavController().navigate(R.id.action_languageFragment_to_onBoardingFragment)
             }
             cvUzbek.setOnClickListener {
+                setLocate("uz")
                 findNavController().navigate(R.id.action_languageFragment_to_onBoardingFragment)
             }
+        }
+    }
+    private fun setLocate(s: String) {
+        val locale = Locale(s)
+        Locale.setDefault(locale)
+
+        val configuration = Configuration()
+        configuration.locale = locale
+
+        requireActivity().baseContext.resources.updateConfiguration(configuration,requireActivity().baseContext.resources.displayMetrics)
+
+        val editor = requireActivity().getSharedPreferences("settings", Context.MODE_PRIVATE).edit()
+        editor.putString("my_language", s)
+        editor.apply()
+
+    }
+
+    private fun loadLocate() {
+        val sharedPreferences = requireActivity().getSharedPreferences("settings", Context.MODE_PRIVATE)
+        val language = sharedPreferences.getString("my_language", "")
+        if (language != null) {
+            setLocate(language)
         }
     }
 }
